4fd8461a7b77e707d1177f6d85bfb2c733f1b493
sifonyarya <81504017+sifonyarya@users.noreply.github.com>
2022-08-31 02:39:21 +0300

    Update Babel22

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const corsOption = {
+    origin:'http://localhost:3000', 
+    credentials:true, 
+}
+app.use(cors(corsOption));
+const PORT = process.env.PORT || 5005
+
+
+app.use(express.json())
+app.use(cookieParser());
+
+app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`))
