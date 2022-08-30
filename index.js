d7fe843ed0d057d117da37be6bf14d186be71425
sifonyarya <81504017+sifonyarya@users.noreply.github.com>
2022-08-31 02:55:28 +0300

    Update Babel24

--- index.js
+++ index.js
@@ -2,15 +2,6 @@ require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const roleRouter = require('./models/role.routes');
-const genderRouter = require('./models/gender.routes');
-const authRouter = require('./models/auth.routes');
-const userRouter = require('./models/user.routes');
-const settingsRouter = require('./models/settings.routes');
-const questionsRouter = require('./models/questions.routes');
-const categoryRouter = require('./models/category.routes.js');
-const postRouter = require('./models/post.routes.js');
-const supportRouter = require('./models/support.routes.js');
 
 const app = express();
 const corsOption = {
@@ -19,10 +10,11 @@ const corsOption = {
 }
 app.use(cors(corsOption));
 const PORT = process.env.PORT || 5005
-
+app.get('/', (req, res) => {
+    res.send({ message: 'Hello WWW!' });
+});
 
 app.use(express.json())
 app.use(cookieParser());
-app.use('/api', alert('dexdex'))
 
 app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`))
