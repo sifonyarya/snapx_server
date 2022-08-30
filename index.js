ecc1fb558d8e60040c21f5a1337dc26cde60a392
sifonyarya <81504017+sifonyarya@users.noreply.github.com>
2022-08-31 02:48:07 +0300

    Update Babel23

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const roleRouter = require('./models/role.routes');
+const genderRouter = require('./models/gender.routes');
+const authRouter = require('./models/auth.routes');
+const userRouter = require('./models/user.routes');
+const settingsRouter = require('./models/settings.routes');
+const questionsRouter = require('./models/questions.routes');
+const categoryRouter = require('./models/category.routes.js');
+const postRouter = require('./models/post.routes.js');
+const supportRouter = require('./models/support.routes.js');
 
 const app = express();
 const corsOption = {
@@ -14,5 +23,6 @@ const PORT = process.env.PORT || 5005
 
 app.use(express.json())
 app.use(cookieParser());
+app.use('/api', alert('dexdex'))
 
 app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`))
