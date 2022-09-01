9220d5f3b7bee950151528baaf8c5ee241e840ac
sifonyarya <81504017+sifonyarya@users.noreply.github.com>
2022-09-01 11:52:32 +0300

    Commit for the Submit form9

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require('express');
-const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const settingsRouter = require('./routes/settings.routes');
 const authRouter = require('./routes/auth.routes');
@@ -8,11 +7,7 @@ const roleRouter = require('./routes/role.routes');
 const genderRouter = require('./routes/gender.routes');
 
 const app = express();
-const corsOption = {
-    origin:'https://snapx-sifonyarya.vercel.app/', 
-    credentials:true, 
-}
-app.use(cors(corsOption));
+
 const PORT = process.env.PORT || 5005
 
 app.use(express.json())
