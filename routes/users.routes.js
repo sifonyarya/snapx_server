2a8abef087be517591c57c0e36dc6cd07c6a4c7a
sifonyarya <81504017+sifonyarya@users.noreply.github.com>
2022-09-25 15:10:15 +0300

    Bug Fixed00008

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -6,7 +6,7 @@ const router = new Router();
 router.post('/users', userContainer.createUser)
 router.get('/users', userContainer.getUsers)
 router.get('/users/:id', userContainer.getOneUser)
-router.put('/users', userContainer.updateUser)
+router.put('/users/:id', userContainer.updateUser)
 router.delete('/users/:id', userContainer.deleteUser)
 
 
